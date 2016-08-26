b05398a947d0635c7e4575d970776062bbb67259
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-08-26 13:45:39 -0300

    Added __meta__ to gitignore
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ import sys
 from invoke import run, task
 
 # Import some boilerplate tasks (see http://... for references)
-from python_boilerplate.tasks.default import *
+from python_boilerplate.tasks import *
 
 
 @task
