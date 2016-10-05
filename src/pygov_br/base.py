93bfdf8457093b4d1973cd797b8a442cc75b71c4
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-10-05 18:39:45 -0300

    Updated deputy client
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/pygov_br/base.py
+++ src/pygov_br/base.py
@@ -49,3 +49,27 @@ class Client(object):
         for element in element_tree.findall(xml_tag):
             element_list.append(element.attrib)
         return element_list
+
+    def _make_dict_from_tree(self, element_tree):
+        def internal_iter(tree, accum):
+            if tree is None:
+                return accum
+
+            if tree.getchildren():
+                accum[tree.tag] = {}
+                for each in tree.getchildren():
+                    result = internal_iter(each, {})
+                    if each.tag in accum[tree.tag]:
+                        if not isinstance(accum[tree.tag][each.tag], list):
+                            accum[tree.tag][each.tag] = [
+                                accum[tree.tag][each.tag]
+                            ]
+                        accum[tree.tag][each.tag].append(result[each.tag])
+                    else:
+                        accum[tree.tag].update(result)
+            else:
+                accum[tree.tag] = tree.text
+
+            return accum
+
+        return internal_iter(element_tree, {})
--- src/pygov_br/camara_deputados/deputy.py
+++ src/pygov_br/camara_deputados/deputy.py
@@ -1,4 +1,5 @@
 from pygov_br.base import Client
+from xml.etree.ElementTree import fromstring, ElementTree
 from xmldict import xml_to_dict
 
 
@@ -9,17 +10,95 @@ class DeputyClient(Client):
         super(DeputyClient, self).__init__(host)
 
     def all(self):
+        """
+        List all deputies acting on Câmara dos Deputados.
+
+        Parameters:
+            None
+        Return:
+            Returns a list of dictionaries containing the information about
+            the deputies.
+        """
         xml_response = self._get('ObterDeputados')
         dict_response = xml_to_dict(xml_response)
         return dict_response['deputados']['deputado']
 
-    def parties(self):
-        xml_response = self._get('ObterPartidosCD')
-        dict_response = xml_to_dict(xml_response)
-        return dict_response['partidos']['partido']
+    def details(self, deputy_id, legislature=''):
+        """
+        List detailed information about a specific deputy.
 
-    def deputy_details(self, deputy_id, legislature=''):
+        Parameters:
+            [Mandatory] deputy_id: Integer
+            [Optional] lesgislature: Integer
+        Return:
+            Returns a list of dictionaries containing extra information about
+            the deputy. Each dictionary represents one legislative period.
+
+        """
         path = 'ObterDetalhesDeputado?ideCadastro={}&numLegislatura={}'
         xml_response = self._get(path.format(deputy_id, legislature))
         dict_response = xml_to_dict(xml_response)
         return dict_response['Deputados']
+
+    def parties(self):
+        """
+        List all parties with representation on Câmara dos Deputados.
+
+        Parameters:
+            None
+        Return:
+            Returns a list of dictionaries containing the information about
+            the parties.
+        """
+        xml_response = self._get('ObterPartidosCD')
+        dict_response = xml_to_dict(xml_response)
+        return dict_response['partidos']['partido']
+
+    def parties_bloc(self, bloc_id='', legislature=''):
+        """
+        List all parties blocs or filtering by legislature and id.
+
+        Parameters:
+            [Optional] bloc_id: Integer
+            [Optional] legislature: Integer
+
+        Return:
+            Returns a list of dictionaries containing bloc information and a
+            list of parties that belongs to the bloc.
+        """
+        path = 'ObterPartidosBlocoCD?numLegislatura={}&idBloco={}'
+        xml_response = self._get(path.format(legislature, bloc_id))
+        dict_response = xml_to_dict(xml_response)
+        return dict_response['blocos']['bloco']
+
+    def parliamentary_seats(self):
+        """
+        List all paliamentary seats.
+
+        Parameters:
+            None
+        Return:
+            Returns a list of dictionaries containing the information about
+            parliamentaries seats.
+        """
+        xml_response = self._get('ObterLideresBancadas')
+        return self._xml_attributes_to_list(xml_response, 'bancada')
+
+    def parliamentary_seat_leaders(self, seat_initials):
+        """
+        List all leaders and vice-leaders of a specific paliamentary seat.
+
+        Parameters:
+            [Mandatory] seat_initials: String
+        Return:
+            Returns a dictionary with two keys: 'lider' and 'vice_lider', where
+            'lider' is a dictionary and 'vice_lider' a list of dictionaries.
+            Both dictionaries contains deputies informations.
+        """
+        xml_response = self._get('ObterLideresBancadas')
+        element_tree = ElementTree(fromstring(xml_response))
+        parliamentary_seat = element_tree.find(
+            "bancada[@sigla='{}']".format(seat_initials)
+        )
+        dict_response = self._make_dict_from_tree(parliamentary_seat)
+        return dict_response['bancada']
