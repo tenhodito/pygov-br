231d18830aa5ce02533b7a21e0fbda04bc3810e0
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-11-04 21:14:46 -0200

    Added datetime conversion to legislative_body client
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/pygov_br/camara_deputados/legislative_body.py
+++ src/pygov_br/camara_deputados/legislative_body.py
@@ -1,4 +1,5 @@
 from pygov_br.base import Client
+from datetime import datetime
 from xml.etree.ElementTree import fromstring, ElementTree
 from xmldict import xml_to_dict
 
@@ -49,18 +50,23 @@ class LegislativeBodyClient(Client):
         dict_response = self._make_dict_from_tree(members)
         return self._safe(dict_response['membros'])
 
-    def schedule(self, legislative_body_id, intial_date='', final_date=''):
+    def schedule(self, legislative_body_id, initial_date='', final_date=''):
         """
         List all scheduled activities of a legislative body.
 
         Parameters:
             [Mandatory] legislative_body_id: Integer
-            [Optional] intial_date: String (dd/mm/yyyy)
-            [Optional] intial_date: String (dd/mm/yyyy)
+            [Optional] initial_date: String (dd/mm/yyyy) or datetime
+            [Optional] final_date: String (dd/mm/yyyy) or datetime
         """
+        if isinstance(initial_date, datetime):
+            initial_date = initial_date.strftime('%d/%m/%Y')
+        if isinstance(final_date, datetime):
+            final_date = final_date.strftime('%d/%m/%Y')
+
         path = "ObterPauta?IDOrgao={}&datIni={}&datFim={}"
         xml_response = self._get(path.format(legislative_body_id,
-                                             intial_date, final_date))
+                                             initial_date, final_date))
         dict_response = xml_to_dict(xml_response)
         return self._safe(dict_response['pauta']['reuniao'])
 
