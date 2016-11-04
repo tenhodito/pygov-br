fc843e37fb250f0d113278fc8fe7bf8537f4e238
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-11-04 21:12:05 -0200

    Added new exception
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/pygov_br/exceptions.py
+++ src/pygov_br/exceptions.py
@@ -2,8 +2,12 @@ from requests.exceptions import HTTPError
 
 
 class ClientError(HTTPError):
-    """ A generic error while attempting to communicate with Discourse """
+    """ A generic error while attempting to communicate with the server """
 
 
 class ClientServerError(ClientError):
     """ The server encountered an error while processing the request """
+
+
+class MissingParameterError(Exception):
+    """ Some parameter is missing """
