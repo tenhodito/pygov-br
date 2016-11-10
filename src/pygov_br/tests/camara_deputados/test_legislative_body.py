1d63e70e46a21e1311aed4f26aa2393f23ff1523
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-11-10 20:42:07 -0200

    Added tests to legislative_body.py
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/pygov_br/tests/camara_deputados/test_legislative_body.py
+++ src/pygov_br/tests/camara_deputados/test_legislative_body.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from pygov_br.camara_deputados import cd
+from datetime import datetime
 import responses
 
 
@@ -78,4 +79,115 @@ def test_legislative_body_members():
         'http://www.camara.leg.br/SitCamaraWS/Orgaos.asmx/ObterMembrosOrgao',
         body=xml_response, status=200)
     assert cd.legislative_bodies.members(1) == expected_dict
+    assert len(responses.calls) == 1
+
+
+@responses.activate
+def test_legislative_body_schedule():
+    xml_response = """
+    <pauta orgao="CDC" dataInicial="01/01/2012" dataFinal="30/04/2012">
+        <reuniao>
+            <comissao>CDC</comissao>
+            <proposicoes>
+                <proposicao>
+                    <sigla>PL 1762/2011</sigla>
+                </proposicao>
+                <proposicao>
+                    <sigla>PL 1390/2012</sigla>
+                </proposicao>
+            </proposicoes>
+        </reuniao>
+        <reuniao>
+            <comissao>CDC</comissao>
+            <proposicoes>
+                <proposicao>
+                    <sigla>PL 1762/2011</sigla>
+                </proposicao>
+                <proposicao>
+                    <sigla>PL 1390/2012</sigla>
+                </proposicao>
+            </proposicoes>
+        </reuniao>
+    </pauta>
+    """
+    expected_list = [
+        {'comissao': 'CDC',
+         'proposicoes': {'proposicao': [{'sigla': 'PL 1762/2011'},
+                                        {'sigla': 'PL 1390/2012'}]}},
+        {'comissao': 'CDC',
+         'proposicoes': {'proposicao': [{'sigla': 'PL 1762/2011'},
+                                        {'sigla': 'PL 1390/2012'}]}}
+    ]
+    responses.add(
+        responses.GET,
+        'http://www.camara.leg.br/SitCamaraWS/Orgaos.asmx/ObterPauta',
+        body=xml_response, status=200)
+    result_list = cd.legislative_bodies.schedule(1, '10/10/2010', '10/10/2010')
+    assert result_list == expected_list
+    assert len(responses.calls) == 1
+
+
+@responses.activate
+def test_legislative_body_schedule_with_datetime():
+    xml_response = """
+    <pauta orgao="CDC" dataInicial="01/01/2012" dataFinal="30/04/2012">
+        <reuniao>
+            <comissao>CDC</comissao>
+            <proposicoes>
+                <proposicao>
+                    <sigla>PL 1762/2011</sigla>
+                </proposicao>
+                <proposicao>
+                    <sigla>PL 1390/2012</sigla>
+                </proposicao>
+            </proposicoes>
+        </reuniao>
+        <reuniao>
+            <comissao>CDC</comissao>
+            <proposicoes>
+                <proposicao>
+                    <sigla>PL 1762/2011</sigla>
+                </proposicao>
+                <proposicao>
+                    <sigla>PL 1390/2012</sigla>
+                </proposicao>
+            </proposicoes>
+        </reuniao>
+    </pauta>
+    """
+    expected_list = [
+        {'comissao': 'CDC',
+         'proposicoes': {'proposicao': [{'sigla': 'PL 1762/2011'},
+                                        {'sigla': 'PL 1390/2012'}]}},
+        {'comissao': 'CDC',
+         'proposicoes': {'proposicao': [{'sigla': 'PL 1762/2011'},
+                                        {'sigla': 'PL 1390/2012'}]}}
+    ]
+    responses.add(
+        responses.GET,
+        'http://www.camara.leg.br/SitCamaraWS/Orgaos.asmx/ObterPauta',
+        body=xml_response, status=200)
+    result_list = cd.legislative_bodies.schedule(1, datetime.now(),
+                                                 datetime.now())
+    assert result_list == expected_list
+    assert len(responses.calls) == 1
+
+
+@responses.activate
+def test_legislative_body_types():
+    xml_response = """
+    <tipoOrgaos>
+        <tipoOrgao id="81000" descricao="Bancada"/>
+        <tipoOrgao id="1000" descricao="Sociedade Civil"/>
+    </tipoOrgaos>
+    """
+    expected_list = [
+        {'id': 81000, 'descricao': 'Bancada'},
+        {'id': 1000, 'descricao': 'Sociedade Civil'},
+    ]
+    responses.add(
+        responses.GET,
+        'http://www.camara.leg.br/SitCamaraWS/Orgaos.asmx/ListarTiposOrgaos',
+        body=xml_response, status=200)
+    assert cd.legislative_bodies.types() == expected_list
     assert len(responses.calls) == 1
